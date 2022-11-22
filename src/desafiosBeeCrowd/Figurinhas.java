4340064034d855b4f50b1fb1fef7b8bb7dc7f0b2
Reinan Marques <wesleremarques@gmail.com>
2022-11-22 13:11:02 -0300

    Figurinha resolução - p

--- src/desafiosBeeCrowd/Figurinhas.java
+++ src/desafiosBeeCrowd/Figurinhas.java
@@ -1,5 +1,39 @@
 package desafiosBeeCrowd;
 
-public class Figurinhas {
+import java.util.Scanner;
 
+public class Figurinhas {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Numero 1:");
+		int nDeCartasJogador = input.nextInt();
+		input.nextLine();
+		System.out.println("Numero 2:");
+		int nDeCartasOponete = input.nextInt();
+		
+		int monte = 0;
+		
+		int maior = nDeCartasJogador > nDeCartasOponete
+				? nDeCartasJogador: nDeCartasOponete;
+		int menor = nDeCartasJogador < nDeCartasOponete
+				? nDeCartasJogador: nDeCartasOponete;
+		
+		boolean objetivo = true;
+		int x = menor; 
+		while(objetivo) {
+			if(maior%x == 0 && menor%x ==0) {
+				monte = x;
+				objetivo = false;
+			}
+			x--;
+		}
+		
+		
+		System.out.println(monte);
+		
+		
+		
+		input.close();
+	}	 
 }
