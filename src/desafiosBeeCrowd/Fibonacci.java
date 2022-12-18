eb5dba19ac9023874b4bf5c0746e56998ab70451
Reinan Marques <wesleremarques@gmail.com>
2022-12-18 11:14:41 -0300

    Camadas de Fibonacci

---
+++ src/desafiosBeeCrowd/Fibonacci.java
@@ -0,0 +1,14 @@
+package desafiosBeeCrowd;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int calls;
+        int numero;
+        Scanner input = new Scanner(System.in);
+        numero = input.nextInt();
+
+        input.close();
+    }
+}
